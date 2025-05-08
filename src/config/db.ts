2c66e754e459cf96aca3cd1c66d1973503ef6bb5
EzequielGomez-code <ezequiel.cepeda.gomez@gmail.com>
2025-05-08 14:54:07 -0400

    Commit_bd_GIT

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import { User } from '../models/User';
 
 import { loadEnvFile } from 'node:process';
+import { RegisterService } from '../models/RegisterService';
 
 
 loadEnvFile()
@@ -12,30 +13,30 @@ if (!process.env.DATABASE_URL) {
 }
 
  //Inicializar conexión a la base de datos
- //const db = new Sequelize(process.env.DATABASE_URL, {
-  //  logging: false,
-  //  dialectOptions: {
-  //    ssl: {
-  //      require: true,
-  //      rejectUnauthorized: false
-  //    },
-  //    connectionTimeoutMillis: 30000  // Timeout de 30 segundos
-  //  }
-  //});
+ const db = new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      connectionTimeoutMillis: 30000  // Timeout de 30 segundos
+    }
+  });
 
 //Inicializar modelos
-//db.addModels([User, Producto, Inventario, UserDetails, UserRecipes, Contact]);
+db.addModels([User, RegisterService]);
 
-const db = new Sequelize(
-    "Grupo6", 
-    'postgres', 
-    '12345678', 
-    {
-        host: 'localhost',
-      dialect: 'postgres',
-      models: [__dirname + '/../models/**/*.ts'],
-      logging: false
-    }
-)
+//const db = new Sequelize(
+    //"Grupo6", 
+    //'postgres', 
+    //'12345678', 
+    //{
+        //host: 'localhost',
+      //dialect: 'postgres',
+      //models: [__dirname + '/../models//*.ts'],
+      //logging: false
+    //}
+//)
 
 export default db;
